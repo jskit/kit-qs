1dd2f9df848205c2d55d9958044108386c60c18a
jack <1395093509@qq.com>
2018-05-03 13:15:22 +0800

    更新

--- src/stringify.js
+++ src/stringify.js
@@ -5,16 +5,34 @@
  * @param {any} params
  * @returns
  */
-function stringify(params) {
+
+// const toISO = Date.prototype.toISOString;
+
+const defaults = {
+  delimiter: '&',
+  // encode: true,
+  // encoder: utils.encode,
+  // encodeValuesOnly: false,
+  // serializeDate: function serializeDate(date) {
+  //   return toISO.call(date);
+  // },
+  // skipNulls: false,
+  // strictNullHandling: false,
+}
+
+function stringify(params, options = {}) {
+  const opts = Object.assign({}, defaults, options)
+  const { delimiter = defaults.delimiter } = opts
+
   const arr = []
-  for (const key in params) {
-    if (!params[key]) {
-      delete params[key]
+  for (const key in opts) {
+    if (!opts[key]) {
+      delete opts[key]
     } else {
-      arr.push(`${key}=${params[key]}`)
+      arr.push(`${key}=${opts[key]}`)
     }
   }
-  return arr.join('&')
+  return arr.join(delimiter)
 }
 
 export default stringify
