5d133edc146ae6e219f806cf5b4305a9a0b14e82
jack <1395093509@qq.com>
2018-09-18 13:23:51 +0800

    新增参考文档

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 // 工具类
+// https://stackoverflow.com/questions/34072598/es6-exporting-importing-in-index-file
+// https://github.com/gajus/create-index
+// https://github.com/leebyron/ecmascript-more-export-from
+
 export copy from './copy'
 export parse from './parse'
 export format from './format'
